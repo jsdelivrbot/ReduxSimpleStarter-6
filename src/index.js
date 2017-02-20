97d3411340d2956a82b93f84f6aed226ffd15b67
Ian Landel <iylandel@gmail.com>
2017-02-21 09:56:05 +1000

    add link state to the video_list_items

--- src/index.js
+++ src/index.js
@@ -14,11 +14,18 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = { videos: [] };
+        
+        this.state = { 
+            videos: [],
+            selectedVideo: null,            
+        };
+        
 
         YTSearch({key: API_KEY, term: 'cats'}, (videos) => {
-           this.setState({ videos })
+           this.setState({ 
+               videos: videos,
+               selectedVideo: videos[0]
+            });
         });
     }
 
@@ -27,10 +34,11 @@ class App extends Component {
             <div>
                 <SearchBar />
                 <VideoDetail 
-                video={this.state.videos[0]}
+                video={this.state.selectedVideo}
                 />
 
                 <VideoList
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                 videos={this.state.videos}
                 />
             </div>
